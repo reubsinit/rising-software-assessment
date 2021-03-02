2fce9f2eea63ccbb91517fd23e2129d7e811ec09
reubsinit <reubenmwilson@outlook.com>
2021-03-02 19:47:03 +1000

    quality: mock data overhall

--- src/mocks/data/index.js
+++ src/mocks/data/index.js
@@ -1,58 +1,194 @@
-export const results = [
+export const assessments = [
   {
-    topic: 'Chord Recognition',
     app: 1,
-    time: 12,
-    questions: 3,
-    marks: 1,
-    maxMarks: 2,
     levels: [
       {
         level: 'Level 1',
-        syllabus: 'VCE Music Performance',
-        syllabusId: 24,
-        time: 2,
-        questions: 2,
-        marks: 1,
-        maxMarks: 2,
+        marks: 12,
+        maxMarks: 12,
+        minutes: 1,
+        questions: 1,
+        syllabusId: 47,
+        syllabusName: 'Complete Syllabus'
       },
       {
         level: 'Level 2',
-        syllabus: 'Complete Syllabus',
-        syllabusId: 15,
-        time: 10,
-        questions: 2,
-        marks: 1,
-        maxMarks: 2,
+        marks: 11,
+        maxMarks: 19,
+        minutes: 4,
+        questions: 1,
+        syllabusId: 47,
+        syllabusName: 'Complete Syllabus'
       },
+      {
+        level: 'Level 3',
+        marks: 25,
+        maxMarks: 25,
+        minutes: 2,
+        questions: 2,
+        syllabusId: 47,
+        syllabusName: 'Complete Syllabus'
+      }
     ],
+    marks: 48,
+    maxMarks: 56,
+    minutes: 7,
+    questions: 4,
+    topic: 'Two-Part Rhythm Dictation'
   },
   {
-    topic: 'Scales',
-    app: 2,
-    time: 12,
-    questions: 3,
-    marks: 1,
-    maxMarks: 2,
+    app: 1,
+    levels: [
+      {
+        level: 'WEBAPP-722 Cho Pro',
+        marks: 0,
+        maxMarks: 7,
+        minutes: 1,
+        questions: 1,
+        syllabusId: 1000001,
+        syllabusName: 'Web Testing'
+      },
+      {
+        level: 'Misc stuff',
+        marks: 74,
+        maxMarks: 88,
+        minutes: 3,
+        questions: 88,
+        syllabusId: 1000001,
+        syllabusName: 'Web Testing'
+      },
+      {
+        level: 'Notation in the answers',
+        marks: 22,
+        maxMarks: 29,
+        minutes: 5,
+        questions: 29,
+        syllabusId: 1000001,
+        syllabusName: 'Web Testing'
+      },
+      {
+        level: 'Count-in metronome etc',
+        marks: 39,
+        maxMarks: 41,
+        minutes: 1,
+        questions: 41,
+        syllabusId: 1000001,
+        syllabusName: 'Web Testing'
+      },
+      {
+        level: 'Free text questions',
+        marks: 0,
+        maxMarks: 8,
+        minutes: 2,
+        questions: 8,
+        syllabusId: 1000001,
+        syllabusName: 'Web Testing'
+      },
+      {
+        level: 'WEBAPP-660 - Clicks',
+        marks: 4,
+        maxMarks: 9,
+        minutes: 3,
+        questions: 9,
+        syllabusId: 1000001,
+        syllabusName: 'Web Testing'
+      },
+      {
+        level: 'WEBAPP-620 - Playback',
+        marks: 2,
+        maxMarks: 3,
+        minutes: 1,
+        questions: 3,
+        syllabusId: 1000001,
+        syllabusName: 'Web Testing'
+      },
+      {
+        level: 'Audio in the answers',
+        marks: 0,
+        maxMarks: 2,
+        minutes: 1,
+        questions: 2,
+        syllabusId: 1000001,
+        syllabusName: 'Web Testing'
+      },
+      {
+        level: 'Sample rate switching',
+        marks: 5,
+        maxMarks: 185,
+        minutes: 2,
+        questions: 7,
+        syllabusId: 1000001,
+        syllabusName: 'Web Testing'
+      },
+      {
+        level: 'Sound fonts',
+        marks: 2,
+        maxMarks: 3,
+        minutes: 30,
+        questions: 3,
+        syllabusId: 1000001,
+        syllabusName: 'Web Testing'
+      },
+      {
+        level: 'Multiselect multiple choice',
+        marks: 12,
+        maxMarks: 30,
+        minutes: 124,
+        questions: 13,
+        syllabusId: 1000001,
+        syllabusName: 'Web Testing'
+      }
+    ],
+    marks: 160,
+    maxMarks: 405,
+    minutes: 173,
+    questions: 204,
+    topic: 'Web Testing Auralia'
+  },
+  {
+    app: 1,
     levels: [
       {
         level: 'Level 1',
-        syllabus: 'VCE Music Performance',
-        syllabusId: 24,
-        time: 2,
-        questions: 2,
-        marks: 1,
-        maxMarks: 2,
+        marks: 5,
+        maxMarks: 6,
+        minutes: 4,
+        questions: 6,
+        syllabusId: 47,
+        syllabusName: 'Complete Syllabus'
       },
       {
         level: 'Level 2',
-        syllabus: 'Complete Syllabus',
-        syllabusId: 15,
-        time: 10,
-        questions: 2,
-        marks: 1,
-        maxMarks: 2,
+        marks: 14,
+        maxMarks: 15,
+        minutes: 2,
+        questions: 15,
+        syllabusId: 47,
+        syllabusName: 'Complete Syllabus'
       },
+      {
+        level: 'Level 1',
+        marks: 38,
+        maxMarks: 47,
+        minutes: 78,
+        questions: 47,
+        syllabusId: 15,
+        syllabusName: 'Complete Syllabus - V5 (legacy)'
+      },
+      {
+        level: 'Level 2',
+        marks: 17,
+        maxMarks: 17,
+        minutes: 5,
+        questions: 17,
+        syllabusId: 15,
+        syllabusName: 'Complete Syllabus - V5 (legacy)'
+      }
     ],
-  },
+    marks: 74,
+    maxMarks: 85,
+    minutes: 89,
+    questions: 85,
+    topic: 'Work Identification'
+  }
 ];
