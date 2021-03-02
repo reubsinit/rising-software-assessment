f01236955b0f453f9a06569244cdcba95c05cdec
reubsinit <reubenmwilson@outlook.com>
2021-03-03 09:42:41 +1000

    config: remove cypress plugin from eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,15 @@
 module.exports = {
   root: true,
   env: {
-    // This config is for front end projects operating in browsers
     browser: true,
-    // For config modules
     node: true,
-    // Todo: use recent versions of JS which will get transpiled by Babel
   },
   extends: [
     'eslint:recommended',
     'plugin:vue/recommended',
     '@vue/prettier',
-    //
-    // Requires:
-    // * eslint-config-prettier
-    // * eslint-prettier
-    // added as dev dependencies.
-    //
-    // prettier/recommended configures both as a single step.
-    //
     'plugin:prettier/recommended',
     'plugin:compat/recommended',
-    'plugin:cypress/recommended',
   ],
   ignorePatterns: ['!/.storybook'],
   rules: {
